2acb58cd3a616f2787a978f225ebd8e90e34a87b
kirill <kirkras7356@gmail.com>
2019-06-04 17:24:44 +0300

    configure hmr and dev server

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React      from 'react';
 import { render } from 'react-dom';
+import { hot }    from 'react-hot-loader/root';
+
+import './style';
 
 const ROOT_NODE = document.getElementById('root');
 
-const App = () => (
-    <div>Hello React!</div>
-);
+const App = hot(() => (
+    <div className="test">Hello, React!!</div>
+));
 
 render(<App />, ROOT_NODE);
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,67 @@
-const path = require('path');
+
+const webpack = require('webpack');
+const path    = require('path');
+
+const HtmlWebpackPlugin    = require('html-webpack-plugin');
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
 
 const PATHS = {
-    entry : path.resolve(__dirname, 'src', 'index.jsx'),
-    output: path.resolve(__dirname, 'dist')
+    src : path.resolve(__dirname, 'src'),
+    dist: path.resolve(__dirname, 'dist')
 };
 
-const config = {
-    entry : PATHS.entry,
-    output: {
-        path    : PATHS.output,
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [ {
-            test   : /\.jsx$/,
-            exclude: /node_modules/,
-            use    : {
-                loader: 'babel-loader'
-            }
-        } ]
+module.exports = function (env) {
+    const isProd = env === 'production';
+    const isDev  = env === 'development';
+
+    function addHash(template, hash) {
+        return isProd
+            ? template.replace(/\.[^.]+$/, `.[${ hash }]$&`)
+            : template;
     }
-};
 
-module.exports = config;
+    return {
+        mode : env,
+        entry: {
+            bundle: [ `${ PATHS.src }/index.jsx`, 'webpack-hot-middleware/client' ],
+        },
+        output: {
+            path      : PATHS.dist,
+            filename  : addHash('js/[name].js', 'hash:8'),
+            publicPath: '/dist/'
+        },
+        devtool      : isDev && 'eval',
+        resolveLoader: {
+            moduleExtensions: [ '-loader' ]
+        },
+        resolve: {
+            extensions: [ '.js', '.jsx', '.css', '.sass', '.scss', '.json' ],
+            alias     : {
+                'react-dom': '@hot-loader/react-dom'
+            }
+        },
+        module: {
+            rules: [ {
+                test   : /\.jsx$/,
+                exclude: /node_modules/,
+                loader : 'babel'
+            }, {
+                test  : /\.css$/,
+                loader: 'style!css',
+            }, {
+                test  : /\.(scss|sass)$/,
+                loader: 'style!css!resolve-url!sass?sourceMap'
+            } ]
+        },
+        watch  : isDev,
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(PATHS.src, 'index.pug'),
+                filename: path.resolve(__dirname, 'views', 'index.pug')
+            }),
+            new HtmlWebpackPugPlugin(),
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NoEmitOnErrorsPlugin()
+        ]
+    };
+};
