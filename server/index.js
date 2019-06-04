827d39b16841a8a806ac24546c4cf0fae7b8ad3d
kirill-krasuk <naviinsane@gmail.com>
2019-06-04 17:24:44 +0300

    configure hmr and dev server

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express       = require('express');
+const webpack       = require('webpack');
+const DevMiddleware = require('webpack-dev-middleware');
+const HotMiddleware = require('webpack-hot-middleware');
+const path          = require('path');
+
+const nodeEnv = process.env.NODE_ENV;
+
+const webpackConfig = require('../webpack.config')(nodeEnv);
+
+const app = express();
+
+const bundler = webpack(webpackConfig);
+
+app.use('/dist', express.static(path.resolve(__dirname, '..', 'dist')));
+
+app.set('view engine', 'pug');
+
+app.use(DevMiddleware(bundler, {
+    publicPath: webpackConfig.output.publicPath,
+    stats     : { colors: true },
+}));
+app.use(HotMiddleware(bundler));
+
+app.use('*', (req, res) => res.render('index'));
+
+app.listen(3000, () => {
+    console.log('Server started');
+});
