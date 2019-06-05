11362a5ba9fc22e98576eee979717c59880229a3
kirill-krasuk <naviinsane@gmail.com>
2019-06-06 00:53:54 +0300

    split webpack config

--- server/index.js
+++ server/index.js
@@ -2,29 +2,38 @@ const express       = require('express');
 const webpack       = require('webpack');
 const DevMiddleware = require('webpack-dev-middleware');
 const HotMiddleware = require('webpack-hot-middleware');
+const compression   = require('compression');
 const path          = require('path');
 const dotenv        = require('dotenv');
 
 dotenv.config();
 
-const nodeEnv = process.env.NODE_ENV;
-const debug   = process.env.DEBUG;
-
-const webpackConfig = require('../webpack.config')({ env: nodeEnv, debug });
-
 const app = express();
 
-const bundler = webpack(webpackConfig);
+const nodeEnv = process.env.NODE_ENV;
+
+app.use(compression());
 
 app.use('/dist', express.static(path.resolve(__dirname, '..', 'dist')));
 
 app.set('view engine', 'pug');
 
-app.use(DevMiddleware(bundler, {
-    publicPath: webpackConfig.output.publicPath,
-    stats     : { colors: true },
-}));
-app.use(HotMiddleware(bundler));
+if (nodeEnv === 'development') {
+    const webpackConfig = require('../webpack/webpack.dev');
+    const bundler       = webpack(webpackConfig);
+
+    app.use(DevMiddleware(bundler, {
+        publicPath: webpackConfig.output.publicPath,
+        stats     : { colors: true },
+    }));
+    app.use(HotMiddleware(bundler));
+}
+
+app.get('*.js', (req, res, next) => {
+    req.url = req.url + '.gz'; // eslint-disable-line
+    res.set('Content-Encoding', 'gzip');
+    next();
+});
 
 app.use('*', (req, res) => res.render('index'));
 
