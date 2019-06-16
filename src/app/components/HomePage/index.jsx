158b577a56d7f499e4562b4bdb7d0bb4ee1e54cc
kirill <kirkras7356@gmail.com>
2019-06-16 14:39:26 +0300

    add helmtet

--- src/app/components/HomePage/index.jsx
+++ src/app/components/HomePage/index.jsx
@@ -1,11 +1,15 @@
-import React     from 'react';
-import { Link }  from 'react-router-dom';
+import React      from 'react';
+import { Link }   from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
-import { hot }   from 'react-hot-loader/root';
+import { hot }    from 'react-hot-loader/root';
 
 const HomePage = () => (
     <>
-        <div>Home Page1</div>
+        <Helmet
+            title="Home page"
+        />
+        <div>Home Page</div>
         <Link to={ '/second' }>go to second page</Link>
     </>
 );
