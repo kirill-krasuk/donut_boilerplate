040ef991f558183afa73e1f4e9d9d9932f448f81
kirill-krasuk <naviinsane@gmail.com>
2019-07-02 15:50:48 +0300

    canUseDOM helper variable

--- src/core/saga/ready.js
+++ src/core/saga/ready.js
@@ -2,9 +2,13 @@
 import { takeEvery } from 'redux-saga/effects';
 import type { Saga } from 'redux-saga';
 
+import { canUseDOM } from 'core/utils/dom';
+
 // eslint-disable-next-line
 function* ready(): Saga<void> {
-    console.log('hello saga');
+    if (canUseDOM) {
+        console.log('!!!!!hello saga and dom has been loaded!!!!!');
+    }
 }
 
 export default function* (): Saga<void> {
