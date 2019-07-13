941faad988b0d9b0570496e99ab7aee567b88927
kirill-krasuk <naviinsane@gmail.com>
2019-07-13 22:44:51 +0300

    add global styles

---
+++ src/core/utils/styles/GlobalStyles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+      margin: 0;
+      padding: 0;
+      font-family: 'Montserrat', sans-serif;
+  }
+`;
