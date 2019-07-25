a4bae3983e5d85e9a1eade0c9366aa15473c3228
kirill <kirkras7356@gmail.com>
2019-07-25 17:50:59 +0300

    add svgo optimize

--- webpack/loaders/svg-loader.js
+++ webpack/loaders/svg-loader.js
@@ -1,12 +1,30 @@
 function getSVGLoader(isClient = true) {
     const SVGLoader = {
         test  : /\.svg$/,
-        loader: 'babel!react-svg?jsx'
+        loader: [
+            'babel',
+            {
+                loader : 'react-svg-loader',
+                options: {
+                    jsx : true,
+                    svgo: {
+                        plugins: [ {
+                            removeTitle               : true,
+                            inlineStyles              : true,
+                            removeEmptyAttrs          : true,
+                            convertColors             : true,
+                            removeUselessStrokeAndFill: true,
+                            removeDimensions          : true,
+                        } ],
+                        floatPrecision: 2
+                    }
+                }
+            }
+        ]
     };
 
     if (isClient) {
-        // SVGLoader.loader.unshift('cache?cacheDirectory=.cache/svg-cache');
-        SVGLoader.loader = `cache?cacheDirectory=.cache/svg-cache!${ SVGLoader.loader }`;
+        SVGLoader.loader.unshift('cache?cacheDirectory=.cache/svg-cache');
     }
 
     return SVGLoader;
