4cd4a21ac578221c5dde36a21600bb786e469d3c
kirill-krasuk <naviinsane@gmail.com>
2019-07-25 14:38:19 +0300

    add image optimizing

--- webpack/loaders/image-loader.js
+++ webpack/loaders/image-loader.js
@@ -1,14 +1,44 @@
 function getImageLoader(isClient = true) {
     const imageLoader = {
-        test   : /\.(gif|png|jpe?g)$/i,
-        loader : 'file',
-        options: {
-            name      : '[name].[ext]',
-            outputPath: '../public/images',
-            publicPath: '/public/images',
-            emit      : !isClient
-        }
+        test  : /\.(gif|png|jpe?g)$/i,
+        loader: [
+            {
+                loader : 'file',
+                options: {
+                    name      : '[name].[ext]',
+                    outputPath: '../public/images',
+                    publicPath: '/public/images',
+                    emit      : !isClient
+                }
+            }
+        ]
     };
+
+    if (isClient) {
+        imageLoader.loader.unshift('cache?cacheDirectory=.cache/images-cache');
+        imageLoader.loader.push({
+            loader : 'image-webpack',
+            options: {
+                mozjpeg: {
+                    progressive: true,
+                    quality    : 65
+                },
+                optipng: {
+                    enabled: process.env.NODE_ENV === 'production'
+                },
+                pngquant: {
+                    quality: '65-90',
+                    speed  : 4
+                },
+                gifsicle: {
+                    interlaced: false
+                },
+                webp: {
+                    quality: 75
+                }
+            }
+        });
+    }
 
     return imageLoader;
 }
