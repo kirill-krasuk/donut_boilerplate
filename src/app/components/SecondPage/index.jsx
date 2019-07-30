6ff6fa2d56d1e9989a08dcfd111197bfd37dd804
kirill <kirkras7356@gmail.com>
2019-07-30 14:25:28 +0300

    change hoc to hook for react-redux

--- src/app/components/SecondPage/index.jsx
+++ src/app/components/SecondPage/index.jsx
@@ -1,18 +1,16 @@
 // @flow
 import React                        from 'react';
 import type { ComponentType, Node } from 'react';
-import { connect }                  from 'react-redux';
-import { compose }                  from 'redux';
+import { useDispatch }              from 'react-redux';
 import { push }                     from 'connected-react-router';
 import { Helmet }                   from 'react-helmet';
 import { hot }                      from 'react-hot-loader/root';
 
 import { callNotification }         from 'core/utils/notification/callNotification';
 import { CurvedHeader }             from 'ui/components';
-import type { PropsType }           from './types';
 
-const SecondPage: ComponentType<PropsType> = (props): Node => {
-    const { push } = props;
+const SecondPage: ComponentType<{}> = (): Node => {
+    const dispatch = useDispatch();
 
     return (
         <>
@@ -21,19 +19,10 @@ const SecondPage: ComponentType<PropsType> = (props): Node => {
             />
             <CurvedHeader />
             <div className="test">Second Page</div>
-            <button onClick={ () => push('/') }>go to main page</button>
+            <button onClick={ () => dispatch(push('/')) }>go to main page</button>
             <button onClick={ () => callNotification({ body: 'Hello from Home Page' }) }>notify me</button>
         </>
     );
 };
 
-const mapDispatchToProps = ({
-    push
-});
-
-const withConnect = connect(null, mapDispatchToProps);
-
-export default compose(
-    hot,
-    withConnect
-)(SecondPage);
+export default hot(SecondPage);
