343f33cefa14af7e3c8859314bfac44e0c3953ec
kirill-krasuk <naviinsane@gmail.com>
2019-08-11 23:48:52 +0300

    change action type constant

--- src/app/sagas/test/index.js
+++ src/app/sagas/test/index.js
@@ -2,8 +2,9 @@
 import { takeEvery }     from 'redux-saga/effects';
 import type { Saga }     from 'redux-saga';
 
+import { READY }         from 'core/actions/ready';
 import { fetchForTest }  from './fetchForTest';
 
 export default function* (): Saga<void> {
-    yield takeEvery('core/READY', fetchForTest);
+    yield takeEvery(READY, fetchForTest);
 }
