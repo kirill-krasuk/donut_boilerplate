cd2680ff1eba27d6ae8c5801624ab7a86b42c3a1
kirill <kirkras7356@gmail.com>
2019-09-03 00:25:44 +0300

    upd fetchAction

--- src/app/sagas/test/fetchForTest.js
+++ src/app/sagas/test/fetchForTest.js
@@ -6,12 +6,22 @@ import api             from 'app/routes/api';
 import { fetchAction } from 'core/actions/fetch';
 
 export function* fetchForTest(): Saga<void> {
+    const handleStartFetch = () => {
+        console.log('start fetch');
+    };
+
+    const handleFinallyFetch = () => {
+        console.log('finally');
+    };
+
     yield put(fetchAction({
-        route         : api.test.getPosts,
-        successHandler: payload => ({
+        route    : api.test.getPosts,
+        onSuccess: payload => ({
             type: 'FETCH_TEST_SUCCESS',
             payload
-        })
+        }),
+        startHandler  : handleStartFetch,
+        finallyHandler: handleFinallyFetch
     }));
 
     const { payload } = yield take('FETCH_TEST_SUCCESS');
