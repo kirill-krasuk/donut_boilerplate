1edee7dd7ce8f9ed706b1e6850d7febeb179363a
kirill-krasuk <naviinsane@gmail.com>
2020-01-13 15:40:30 +0200

    reduce intl size

--- src/core/components/LanguageProvider/index.jsx
+++ src/core/components/LanguageProvider/index.jsx
@@ -3,15 +3,11 @@
 import React                           from 'react';
 import type { ComponentType, Node }    from 'react';
 import { useSelector }                 from 'react-redux';
-import { IntlProvider, addLocaleData } from 'react-intl';
-import en                              from 'react-intl/locale-data/en';
-import ru                              from 'react-intl/locale-data/ru';
+import { IntlProvider }                from 'react-intl';
 
 import messages                        from 'core/locales';
 import { getLocale }                   from 'core/selectors/locale';
 import type { PropsType }              from './types';
-
-addLocaleData([ ...en, ...ru ]);
 
 const LanguageProvider: ComponentType<PropsType> = (props): Node => {
     const { children } = props;
--- webpack/loaders/js-loader.js
+++ webpack/loaders/js-loader.js
@@ -2,7 +2,7 @@ function getJsLoader(isClient = true) {
     const jsLoader = {
         test   : /\.jsx?$/,
         loader : 'babel',
-        exclude: [ /node_modules/, /\.(spec|test)\.js$/ ]
+        exclude: [ /node_modules/, /\.(spec|test)\.js$/, /node_modules\/(?!react-intl|intl-messageformat|intl-messageformat-parser)/ ]
     };
 
     if (isClient && process.env.BABEL_ENV === 'development') {
