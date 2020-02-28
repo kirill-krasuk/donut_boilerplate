6b2aef645467978e2831845d4dcef85a32fed6d2
kirill-krasuk <naviinsane@gmail.com>
2020-02-29 00:29:41 +0200

    configure jest for ts and write first test

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,8 @@ module.exports = {
         '.tsx?$': 'ts-jest'
     },
     setupFiles: [
-        './src/test/setup.js',
-        './src/test/shim.js'
+        './src/test/setup.ts',
+        './src/test/shim.ts'
     ],
     moduleFileExtensions: [
         'ts',
@@ -16,11 +16,22 @@ module.exports = {
         'jsx',
         'json',
     ],
+    testRegex: [
+        '(/src/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$'
+    ],
     snapshotSerializers: [
         '<rootDir>/node_modules/enzyme-to-json/serializer'
     ],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/test/__mocks__/fileMock.js',
         '/\.module\.s(c|a)ss$/'                                                              : 'identity-obj-proxy',
+    },
+    globals: {
+        // we must specify a custom tsconfig for tests because we need the typescript transform
+        // to transform jsx into js rather than leaving it jsx such as the next build requires. you
+        // can see this setting in tsconfig.jest.json -> "jsx": "react"
+        'ts-jest': {
+            tsConfig: '<rootDir>/tsconfig.jest.json'
+        }
     }
 };
