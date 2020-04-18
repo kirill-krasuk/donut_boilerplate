77ba8c8ba49ed9a00a9111776fd6f0850a3a66c8
kirill <kirkras7356@gmail.com>
2020-04-18 06:24:47 +0300

    refactoring redux types

--- src/core/middlewares/locale.ts
+++ src/core/middlewares/locale.ts
@@ -1,12 +1,11 @@
+import { Middleware }    from 'redux';
 import Cookie            from 'js-cookie';
 
-import { CHANGE_LOCALE } from '@core/actions/locale';
 import { Action }        from '@core/types/actions';
+import { CHANGE_LOCALE } from '@core/actions/locale';
 
-export const localeMiddleware = () => (next: Function) => (action: Action): Function => {
-    if (action.type === CHANGE_LOCALE) {
-        Cookie.set('locale', action.payload);
-    }
-
-    return next(action);
-};
+export const localeMiddleware: Middleware = () => (next: Function) => (action: Action): Function => (
+    action.type === CHANGE_LOCALE
+        ? Cookie.set('locale', action.payload)
+        : next(action)
+);
