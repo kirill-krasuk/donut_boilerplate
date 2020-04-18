accae5302b45e8a51bbeeb112b9cf687bcf302f4
kirill <kirkras7356@gmail.com>
2020-04-18 07:03:07 +0300

    reduce code

--- src/core/middlewares/theme.ts
+++ src/core/middlewares/theme.ts
@@ -4,14 +4,8 @@ import Cookie           from 'js-cookie';
 import { Action }       from '@core/types/actions';
 import { CHANGE_THEME } from '@core/actions/theme';
 
-const setTheme = (action: Action, next: Function): Function => {
-    Cookie.set('theme', action.payload);
-
-    return next(action);
-};
-
 export const themeMiddleware: Middleware = () => (next: Function) => (action: Action): Function => (
     action.type === CHANGE_THEME
-        ? setTheme(action, next)
+        ? (Cookie.set('theme', action.payload), next(action))
         : next(action)
 );
