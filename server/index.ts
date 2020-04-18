97170d7873adc87b95d807c2b11e7e3b72ea907b
kirill <kirkras7356@gmail.com>
2020-04-18 07:25:17 +0300

    decompose server index file

--- server/index.ts
+++ server/index.ts
@@ -7,49 +7,40 @@ require('@babel/register')({
     ]
 });
 
-import express           from 'express';
-import shrinkRay         from 'shrink-ray-current';
-import favicon           from 'serve-favicon';
-import cookieParser      from 'cookie-parser';
-import bodyParser        from 'body-parser';
-import path              from 'path';
-import dayjs             from 'dayjs';
-import processImage      from 'express-processimage';
-import expressStaticGzip from 'express-static-gzip';
+import express      from 'express';
+import shrinkRay    from 'shrink-ray-current';
+import favicon      from 'serve-favicon';
+import cookieParser from 'cookie-parser';
+import bodyParser   from 'body-parser';
+import path         from 'path';
+import dayjs        from 'dayjs';
+import processImage from 'express-processimage';
 import '@babel/polyfill';
 
-import { ONE_MONTH_CACHE }     from './constants/cache';
 import config                  from './config';
 import { serverSideRendering } from './middlewares/serverSideRendering';
 import { errorLogging }        from './middlewares/errorLogging';
 import { useDevMiddlewares }   from './middlewares/useDevMiddlewares';
-import { useStatic }           from './utils/useStatic';
+import { setStatic }           from './utils/setStatic';
 
 const { host, port } = config;
 
 const app = express();
 
 app.use(shrinkRay());
-
 app.use(processImage());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/dist', useStatic(`${ __dirname  }/../dist`, { maxAge: ONE_MONTH_CACHE }));
-app.use('/sw.js', useStatic(`${ __dirname  }/../dist/sw.js`));
-app.use('/public', expressStaticGzip(path.resolve(`${ __dirname  }/../public`), { enableBrotli: true }));
-app.use('/images', useStatic(`${ __dirname  }/../public/images`, { maxAge: ONE_MONTH_CACHE }));
-app.use('/', useStatic(`${ __dirname  }/../public/root`));
+setStatic(app);
 
 app.use(favicon(path.join(__dirname, '..', '/public/images/favicon.ico')));
-
 app.set('view engine', 'pug');
 
 useDevMiddlewares(app);
 
 app.use('/handle_error', errorLogging);
-
 app.use('/', serverSideRendering);
 
 app.listen(+port, (host as string), () => {
