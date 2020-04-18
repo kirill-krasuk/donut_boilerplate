d07d2a2ca53ec29516833b26f27c8da56fca15d9
kirill-krasuk <naviinsane@gmail.com>
2020-04-18 06:24:47 +0300

    refactoring redux types

--- src/core/types/store.ts
+++
@@ -1,8 +0,0 @@
-import { Action } from '@core/types/actions';
-
-export type Store = {
-    dispatch: Function;
-    getState: Function;
-}
-
-export type Middleware = (store: Store) => (next: Function) => (action: Action) => Function;
