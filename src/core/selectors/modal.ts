5a94d9ef904162d98da6e7549a261f378bb3072f
kirill-krasuk <naviinsane@gmail.com>
2020-04-18 22:35:48 +0300

    monadic selector

--- src/core/selectors/modal.ts
+++ src/core/selectors/modal.ts
@@ -1,17 +1,20 @@
 import { createSelector } from 'reselect';
 import R                  from 'ramda';
+import * as O             from 'fp-ts/lib/Option';
 
 import { Selector }       from '@core/types/selector';
 import { ModalState }     from '@core/types/modal';
+import { EModals }        from '@app/enums/modal';
 
 const selectModal: Selector<ModalState> = R.prop('modal');
 
-export const getModalId = createSelector<object, ReturnType<typeof selectModal>, ModalState['id']>(
-    [ selectModal ],
-    R.prop('id')
+export const getModalIdOption = createSelector<object, ReturnType<typeof selectModal>, O.Option<EModals>>(
+    selectModal,
+    (state) => O.fromNullable(R.prop('id')(state))
 );
 
-export const getHasModal = createSelector<object, ReturnType<typeof getModalId>, boolean>(
-    getModalId,
+// TODO: check this method
+export const getHasModal = createSelector<object, ReturnType<typeof getModalIdOption>, boolean>(
+    getModalIdOption,
     R.complement(R.isNil)
 );
