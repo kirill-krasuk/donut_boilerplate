964234f29d4480bcc5c2eff6901228d58b829db4
kirill <kirkras7356@gmail.com>
2020-04-21 00:08:14 +0300

    delete old implementations of socket

--- src/core/actions/socket/socketInit.ts
+++
@@ -1,6 +0,0 @@
-import { createAction } from 'typesafe-actions';
-
-import { SocketInit }   from '@core/types/socket';
-
-export const SOCKET_INIT: SocketInit['type'] = 'core/SOCKET_INIT';
-export const socketInitAction = createAction(SOCKET_INIT)<SocketInit['payload']>();
