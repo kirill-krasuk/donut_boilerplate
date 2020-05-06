82c8858d482d763f1d6b2d9bdd552da454917e95
kirill <kirkras7356@gmail.com>
2020-05-06 10:52:22 +0300

    add node env to config

--- src/core/types/env.ts
+++ src/core/types/env.ts
@@ -4,10 +4,12 @@ export type Env = {
     apiHost: O.Option<string>;
     swEnable: O.Option<boolean>;
     needHydrate: O.Option<boolean>;
+    appEnv: O.Option<string>;
 }
 
 export type FoldedEnv = {
     apiHost: string;
     swEnable: boolean;
     needHydrate: boolean;
+    appEnv: string;
 }
