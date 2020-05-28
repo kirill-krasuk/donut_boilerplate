35125c03e25ed9d428010a5b94e410794adb0b73
kirill <kirkras7356@gmail.com>
2020-05-28 15:19:02 +0300

    fix store types

--- src/core/utils/store/types.ts
+++ src/core/utils/store/types.ts
@@ -1,5 +1,11 @@
-import { Store } from 'redux';
+import { Store, Reducer } from 'redux';
 
-export type ExtendedStore = Store<any, any> & { asyncReducers: Record<string, any>; injectReducer: Function };
+export type AsyncReducer = Reducer<any, any>;
+
+export type AsyncReducers = {
+    [key: string]: AsyncReducer;
+}
+
+export type ExtendedStore = Store<any, any> & { asyncReducers: AsyncReducers; injectReducer: AsyncReducer };
 
 export type InjectedReducersType = { [key: string]: Record<string, any> };
