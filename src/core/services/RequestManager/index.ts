5b2256fc9bbf6bbfbcc65bbaab7f03d5c76661ca
kirill <kirkras7356@gmail.com>
2020-05-28 23:50:11 +0300

    fix request manager

--- src/core/services/RequestManager/index.ts
+++ src/core/services/RequestManager/index.ts
@@ -6,7 +6,9 @@ import {
 } from 'rxjs';
 import { fromFetch }                                 from 'rxjs/fetch';
 import { switchMap }                                 from 'rxjs/operators';
+import Cookie                                        from 'js-cookie';
 import * as O                                        from 'fp-ts/lib/Option';
+import * as IO                                       from 'fp-ts/lib/IO';
 import { pipe }                                      from 'fp-ts/lib/pipeable';
 import R                                             from 'ramda';
 
@@ -14,7 +16,14 @@ import { HTTPMethod }                                from '@core/enums/http';
 import * as Env                                      from '@core/config/env';
 import { DefaultHeaders, UrlORRequest, RequestBody } from '@core/types/HTTP';
 
+const DEFAULT_HEADERS = {
+    'Content-Type': 'application/json',
+    Accept        : 'application/json'
+};
+
 const apiHost = () => Env.get('apiHost');
+
+const getToken: IO.IO<O.Option<string>> = () => O.fromNullable(Cookie.get('token'));
 
 const setUrl = (route?: string) => pipe(
     O.fromNullable(route),
@@ -24,23 +33,27 @@ const setUrl = (route?: string) => pipe(
     )
 );
 
-const setHeaders = (headers?: object | DefaultHeaders) => {
-    const defaultHeaders = {
-        'Content-Type': 'application/json',
-        Accept        : 'application/json'
-    };
+const getDefaultHeaders = () => pipe(
+    getToken(),
+    O.fold(
+        () => DEFAULT_HEADERS,
+        (token) => ({
+            ...DEFAULT_HEADERS,
+            Authorization: `Bearer ${ token }`
+        })
+    )
+);
 
-    return pipe(
-        O.fromNullable(headers),
-        O.fold(
-            () => defaultHeaders,
-            (headers) => R.mergeDeepRight(
-                defaultHeaders,
-                headers
-            )
+const setHeaders = (headers?: object | DefaultHeaders) => pipe(
+    O.fromNullable(headers),
+    O.fold(
+        () => getDefaultHeaders(),
+        (headers) => R.mergeDeepRight(
+            getDefaultHeaders(),
+            headers
         )
-    );
-};
+    )
+);
 
 const getRequestPayload = (urlOrRequest: UrlORRequest) => (
     typeof urlOrRequest === 'string'
@@ -56,7 +69,7 @@ const getRequestPayload = (urlOrRequest: UrlORRequest) => (
 
 const getUrl = (urlOrRequest: UrlORRequest) => (typeof urlOrRequest === 'string' ? urlOrRequest : urlOrRequest.url);
 
-const request$ = <T = any>(urlOrRequest: UrlORRequest): Observable<string | { response: Response; data: T }> => fromFetch(
+const request$ = <T = any>(urlOrRequest: UrlORRequest): Observable<{ response: Response; data: T }> => fromFetch(
     setUrl(getUrl(urlOrRequest)),
     getRequestPayload(urlOrRequest)
 ).pipe(
