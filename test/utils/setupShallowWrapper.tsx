f9d04b44ba1401b7870fb1ba4f72ee2bf023c1b3
kirill-krasuk <naviinsane@gmail.com>
2020-06-29 22:30:30 +0300

    refactoring test dir

--- test/utils/setupShallowWrapper.tsx
+++ test/utils/setupShallowWrapper.tsx
@@ -2,7 +2,7 @@ import React        from 'react';
 import { Provider } from 'react-redux';
 import { shallow }  from 'enzyme';
 
-import { store }    from '@test/__mocks__/store';
+import { store }    from '../__mocks__/store';
 
 const setupShallowWrapper = (Component: any, provideProps?: Record<string, any>): Record<string, any> => {
     const props = provideProps || {};
