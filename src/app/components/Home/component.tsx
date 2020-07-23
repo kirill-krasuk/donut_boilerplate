60985da52f35c9b50a662e9bb30ec295eb135374
kirill <kirkras7356@gmail.com>
2020-07-23 14:30:59 +0300

    fix utils path

--- src/app/components/Home/component.tsx
+++ src/app/components/Home/component.tsx
@@ -1,16 +1,14 @@
 import React             from 'react';
 import { EOLocale as T } from 'eo-locale';
 import { hot }           from 'react-hot-loader/root';
-import * as RD           from '@devexperts/remote-data-ts';
 
 import { routes }        from '@app/routes/routes';
 import Header            from '@app/components/Header';
-import { Post }          from '@app/types/posts';
 import * as S            from './styled';
 import messages          from './messages';
 import { Props }         from './types';
 
-const Home: React.FC<Props> = (props) => (
+const Home: React.FC<Props> = () => (
     <>
         <Header />
         <S.Container>
@@ -20,14 +18,6 @@ const Home: React.FC<Props> = (props) => (
             <S.Link to={ routes.second.path }>
                 <T.Text id={ messages.link } />
             </S.Link>
-            { (function () {
-                return RD.fold(
-                    () => null,
-                    () => null,
-                    () => null,
-                    (posts: Post[]) => posts.map(post => <div key={ post.id }>{ post.title }</div>)
-                )(props.posts || {});
-            }()) }
         </S.Container>
     </>
 );
