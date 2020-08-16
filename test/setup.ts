909962d90c0012f312925e9ddf5cbb8b270d6834
kirill <kirkras7356@gmail.com>
2020-08-16 21:37:49 +0300

    fix env reading in test environment

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,10 @@
 import Enzyme  from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import jsdom   from 'jsdom';
+import dotenv  from 'dotenv';
+
+// for getEnv helper
+dotenv.config();
 
 const { JSDOM }    = jsdom;
 const { document } = (new JSDOM('<!doctype html><html><body></body></html>')).window;
