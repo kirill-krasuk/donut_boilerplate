f1c840ca2854c627cbdaf62ed6fa22685c443cd4
kirill <naviinsane@gmail.com>
2020-10-10 05:06:09 +0300

    write ci bash script

---
+++ scripts/ci.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NODE_VERSION=$(node -v)
+NPM_VERSION=$(npm -v)
+
+echo used node version ${NODE_VERSION}
+echo used npm version ${NPM_VERSION}
+
+# run next command if previos success
+node_modules/.bin/tsc --noEmit -p . &&
+node_modules/.bin/eslint . &&
+node_modules/.bin/jest --env=jsdom &&
+node_modules/.bin/stylelint './src/**/*.ts'
+
+if [ $? -eq 1 ]; then
+    echo 🚨🚨🚨 CI failed 🚨🚨🚨
+    exit 1
+else
+    echo ✨✨✨CI passed in ${SECONDS} seconds✨✨✨
+    exit 0
+fi
