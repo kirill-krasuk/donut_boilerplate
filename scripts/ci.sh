6d44d12228bf140228d374681df01d13673779cf
kirill <kirkras7356@gmail.com>
2020-10-14 01:27:30 +0300

    add directory check to ci script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -6,6 +6,12 @@ NPM_VERSION=$(npm -v)
 echo used node version ${NODE_VERSION}
 echo used npm version ${NPM_VERSION}
 
+if [ ! -d "./node_modules/" ]; then
+    echo node_modules not found
+    echo check if dependencies are installed
+    exit 1
+fi
+
 # run next command if previos success
 ./node_modules/.bin/tsc --noEmit -p . &&
 ./node_modules/.bin/eslint . &&
