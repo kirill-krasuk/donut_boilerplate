4d3002ce72e56d32b4479a075b705951f01f3987
kirill-krasuk <naviinsane@gmail.com>
2020-10-21 19:07:37 +0300

    delete unused polyfill

--- server/shim.ts
+++ server/shim.ts
@@ -12,7 +12,6 @@ require('@babel/polyfill');
 global.fetch = require('node-fetch');
 require('abortcontroller-polyfill/dist/polyfill-patch-fetch');
 
-require('@server/polyfills/promise');
 require('@server/polyfills/array');
 
 // @ts-expect-error
