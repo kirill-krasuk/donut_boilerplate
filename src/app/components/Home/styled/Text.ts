0fbfab197961fda8fd3b17b4d8a859e44a8b0b18
kirill <kirkras7356@gmail.com>
2020-10-23 00:29:20 +0300

    change theme location in project

--- src/app/components/Home/styled/Text.ts
+++ src/app/components/Home/styled/Text.ts
@@ -5,7 +5,7 @@ import { Theme }       from '@core/types/theme';
 export const Text = styled.span`
     ${ ({ theme }: Theme) => css`
         font-weight: 500;
-        color: ${ theme.common.white };
+        color: ${ theme[theme.mode].typography.text };
         font-size: 1.42rem;
         margin-bottom: 10px;
     ` };
