41758d9944f24b05ad585d6d012c0ffb5108db68
kirill-krasuk <kirkras7356@gmail.com>
2021-01-09 22:31:52 +0200

    update scripts

---
+++ bin/install_deps_dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# this is npm context, because this script
+# must running from package.json
+
+if [ ! -d "./node_modules/" ]; then 
+    if [ -d "./.cache" ]; then
+        bash ./bin/prune_caches.sh
+    fi
+
+    echo "⚠️ Dependencies not installed"
+    echo -e "\nInstalling...\n"
+
+    npm ci && sleep 1 &&
+
+    echo -e "\n✅ Dependencies installed successfully\n"
+fi
---
+++ bin/run_prod.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# echo -e "\n Installing sharp deps \n" && \
+# npm install sharp && \
+echo -e "\n🍩 Start application 🍩\n" && \
+node ./dist/server.js 2>> ./logs/error_build.log
