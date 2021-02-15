b85d25c3caf76c677fa93821f6f32a95791a27ff
kirill-krasuk <naviinsane@gmail.com>
2021-02-15 09:18:43 +0200

    refactoring open in browser method

--- server/utils/appOutput.ts
+++ server/utils/appOutput.ts
@@ -1,5 +1,6 @@
 import dayjs           from 'dayjs';
 import chalk           from 'chalk';
+import openBrowser     from 'react-dev-utils/openBrowser';
 
 import env             from '@server/config';
 import { getHostname } from './getHostname';
@@ -12,17 +13,27 @@ export type OutputInfo = {
 const { isBuildAnalyzer, analyzerPort } = env;
 const isDev                             = env.env === 'development';
 
-export const getAppOutputInfo = ({ host, port }: OutputInfo) => [
-    `      ${ chalk.bold('Welcome to DONUT BOILERPLATE 🍩🍩🍩') }`,
-    '',
-    `${ chalk.green.bold('Server time:') }     ${ dayjs(Date.now()).format('HH:mm:ss DD:MM:YYYY') }`,
-    '',
-    `            ${ chalk.gray.bold('Server started at') }`,
-    `${ chalk.green.bold('Address:') }         ${ chalk.underline.cyan(`http://${ getHostname(host) }:${ port }`) }`,
-    isDev && isBuildAnalyzer && `${ chalk.green.bold('Bundle analyzer:') } ${ chalk.underline.cyan(`http://${ getHostname(host) }:${ analyzerPort }`) }`,
-    '',
-    chalk.gray.bold('Copy address to clipboard and run it in browser')
-]
-    .filter(row => row !== false)
-    .map(row => `${ row }\n`)
-    .join('');
+export const getAppOutputInfo = ({ host, port }: OutputInfo) => {
+    let messageAboutBrowser = 'Copy address to clipboard and run it in browser';
+
+    if (env.isOpenInBrowser) {
+        if (openBrowser(`http://${ getHostname(host) }:${ port }`)) {
+            messageAboutBrowser = 'The app has been opened in browser!';
+        }
+    }
+
+    return [
+        `      ${ chalk.bold('Welcome to DONUT BOILERPLATE 🍩🍩🍩') }`,
+        '',
+        `${ chalk.green.bold('Server time:') }     ${ dayjs(Date.now()).format('HH:mm:ss DD:MM:YYYY') }`,
+        '',
+        `            ${ chalk.gray.bold('Server started at') }`,
+        `${ chalk.green.bold('Address:') }         ${ chalk.underline.cyan(`http://${ getHostname(host) }:${ port }`) }`,
+        isDev && isBuildAnalyzer && `${ chalk.green.bold('Bundle analyzer:') } ${ chalk.underline.cyan(`http://${ getHostname(host) }:${ analyzerPort }`) }`,
+        '',
+        chalk.gray.bold(messageAboutBrowser)
+    ]
+        .filter(row => row !== false)
+        .map(row => `${ row }\n`)
+        .join('');
+};
