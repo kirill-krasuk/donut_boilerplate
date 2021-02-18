98b632d49cb720a5356ac43324b26a4319080402
kirill-krasuk <kirkras7356@gmail.com>
2021-02-18 11:31:15 +0200

    fix file name

--- bin/run_server.sh
+++ bin/run_server.sh
@@ -4,4 +4,4 @@ source ./bin/colors.sh
 
 echo -e "\n${BBlack}${On_Green} START ${Color_Off} 🍩 application 🍩\n" && \
 
-yarn node ./dist/server.js 2> ./logs/server_errrors.log
+yarn node ./dist/server.js 2> ./logs/server_errors.log
