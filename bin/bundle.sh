3fe3204ae466bb0c94130ea2d1cbc0b31870f08e
kirill-krasuk <kirkras7356@gmail.com>
2021-02-19 06:09:10 +0200

    add error handling to bundling

--- bin/bundle.sh
+++ bin/bundle.sh
@@ -24,3 +24,11 @@ echo -e "\n${BBlack}${On_Blue} INFO ${Color_Off} 🔧 Building production client
 BABEL_ENV=production NODE_ENV=production \
 yarn ts-node -P $TOOLS_TS_CONFIG $BUNDLE_TOOL_PATH -c $WEBPACK_CLIENT_CFG \
 2> ./logs/bundle_errors_2.log
+
+if [ $? -eq 1 ]; then
+    echo -e "🚨🚨🚨 Bundling ${BRed}failed${Color_Off} 🚨🚨🚨"
+    exit 1
+else
+    echo -e "✨✨✨ Bundling ${BGreen}passed${Color_Off} ✨✨✨"
+    exit 0
+fi
