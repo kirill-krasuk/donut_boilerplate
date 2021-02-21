0717965bde2acf1e60d5dabb4417fbfc99b0b95e
kirill-krasuk <naviinsane@gmail.com>
2021-02-21 11:41:18 +0200

    speed up testing

--- config/jest/setup.ts
+++ config/jest/setup.ts
@@ -1,7 +1,5 @@
-import Enzyme  from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import jsdom   from 'jsdom';
-import dotenv  from 'dotenv';
+import jsdom  from 'jsdom';
+import dotenv from 'dotenv';
 
 // for getEnv helper
 dotenv.config();
@@ -12,8 +10,6 @@ global.document    = document;
 
 // @ts-ignore
 global.window = document.defaultView;
-
-Enzyme.configure({ adapter: new Adapter() });
 
 // eslint-disable-next-line
 console.error = (message: any) => {
--- test/types/wrapper.ts
+++
@@ -1,5 +0,0 @@
-import { ShallowWrapper as EnzymeShallowWrapper, ReactWrapper } from 'enzyme';
-
-export type ShallowWrapper = EnzymeShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
-export type MountWrapper = ReactWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
-export type Wrapper = ShallowWrapper | MountWrapper;
