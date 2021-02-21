9bf26f809ec546ff518713027b47dc9f555d668a
kirill-krasuk <naviinsane@gmail.com>
2021-02-21 11:45:49 +0200

    fix trash after enzyme deleting

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -1,5 +1,3 @@
-export { setupShallowWrapper } from './setupShallowWrapper';
-export { setupMountWrapper } from './setupMountWrapper';
 export { setupRenderWrapper } from './setupRenderWrapper';
 export { getById } from './getById';
 export { render } from './render';
