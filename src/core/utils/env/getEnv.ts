521da1605eaa936522af89edfad01b3d92b7fb83
kirill-krasuk <naviinsane@gmail.com>
2021-03-11 23:09:02 +0200

    fix env config

--- src/core/utils/env/getEnv.ts
+++ src/core/utils/env/getEnv.ts
@@ -1,20 +1,10 @@
-import * as IO       from 'fp-ts/lib/IO';
-import * as E        from 'fp-ts/lib/Either';
-import * as O        from 'fp-ts/lib/Option';
-import * as C        from 'fp-ts/lib/Console';
-import { pipe }      from 'fp-ts/lib/pipeable';
-import { flow }      from 'fp-ts/lib/function';
+import * as IO  from 'fp-ts/lib/IO';
+import * as E   from 'fp-ts/lib/Either';
+import * as O   from 'fp-ts/lib/Option';
+import * as C   from 'fp-ts/lib/Console';
+import { pipe } from 'fp-ts/lib/pipeable';
 
-import { parseJSON } from '@utils/json';
-
-const dropEmptyValue = O.fromPredicate<string>(Boolean);
-
-const parseOtherValues = flow(
-    O.map(value => pipe(
-        parseJSON(value as string),
-        E.getOrElse(() => value),
-    )),
-);
+const dropEmptyValue = O.fromPredicate((value) => value !== '');
 
 export const getEnv = (getEnv: IO.IO<NodeJS.ProcessEnv>) => <T = string>(key: string, defaultValue?: T) => pipe(
     E.fromNullable(new Error(`Such a variable ${ key } does not exist`))(getEnv()[key]),
@@ -23,11 +13,11 @@ export const getEnv = (getEnv: IO.IO<NodeJS.ProcessEnv>) => <T = string>(key: st
         either,
         E.fold(
             (reason) => (C.error(reason)(), O.none),
-            (value) => parseOtherValues(value) as O.Option<T>
+            (value) => value
         ),
     ),
     O.fold(
         () => O.fromPredicate<T | undefined>(Boolean)(defaultValue) as O.Option<T>,
         (value) => O.some(value)
-    )
+    ),
 );
