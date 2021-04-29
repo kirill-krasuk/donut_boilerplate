bf4973154bc1b919d57336a5f5207ff5418b321e
kirill-krasuk <naviinsane@gmail.com>
2021-04-29 20:44:08 +0300

    rename "tools" for cli to "cli" directory

--- bin/start.sh
+++ bin/start.sh
@@ -11,7 +11,7 @@ source $CONTEXT/colors.sh
 echo -e "${BBlack}${On_Blue} INFO ${Color_Off} 🔧 Building development version\n"
 
 BABEL_CACHE_PATH=$BABEL_CACHE_PATH  NODE_ENV=development \
-ts-node -P $TOOLS_TS_CONFIG ./tools/scripts/bundle.ts -c ./config/webpack/webpack.server.ts \
+ts-node -P $TOOLS_TS_CONFIG ./cli/scripts/bundle.ts -c ./config/webpack/webpack.server.ts \
 2> ./logs/server_bundle.error.log
 
 echo -e "\n${BBlack}${On_Green} START ${Color_Off} 🍩 application 🍩\n" && \
--- bin/variables.sh
+++ bin/variables.sh
@@ -1,13 +1,13 @@
 CONTEXT=./bin
 
-TOOLS_TS_CONFIG=./tools/tsconfig.json
-BUNDLE_TOOL_PATH=./tools/scripts/bundle.ts
+TOOLS_TS_CONFIG=./cli/tsconfig.json
+BUNDLE_TOOL_PATH=./cli/scripts/bundle.ts
 JEST_CONFIG_PATH=./config/jest/jest.config.js
 
 WEBPACK_SERVER_CFG=./config/webpack/webpack.server.ts
 WEBPACK_CLIENT_CFG=./config/webpack/webpack.prod.ts
 
-TOOLS_TS_CONFIG=./tools/tsconfig.json
+TOOLS_TS_CONFIG=./cli/tsconfig.json
 
 BABEL_CACHE_PATH=./.cache/babel-register
 
