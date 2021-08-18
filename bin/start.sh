a142a855cb3fedd5acbf490ba2b459e3b47ad880
kirill-krasuk <naviinsane@gmail.com>
2021-08-18 21:34:47 +0300

    remove redirect command stderr to file

--- bin/start.sh
+++ bin/start.sh
@@ -12,9 +12,9 @@ echo -e "${BBlack}${On_Blue} INFO ${Color_Off} 🔧 Building development version
 
 BABEL_CACHE_PATH=$BABEL_CACHE_PATH  NODE_ENV=development \
 ts-node -P $TOOLS_TS_CONFIG ./cli/scripts/bundle.ts -c ./config/webpack/webpack.server.ts \
-2> ./logs/server_bundle.error.log
+./logs/server_bundle.error.log
 
 echo -e "\n${BBlack}${On_Green} START ${Color_Off} 🍩 application 🍩\n" && \
 BABEL_CACHE_PATH=$BABEL_CACHE_PATH  BABEL_ENV=development NODE_ENV=development \
 yarn node ./dist/server.js \
-2> ./logs/run_server.error.log
+./logs/run_server.error.log
