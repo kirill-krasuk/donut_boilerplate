8b839ba802fff0cc46b784ee044f8c979dc31ed5
kirill-krasuk <kirkras7356@gmail.com>
2021-09-20 23:49:50 +0300

    build: upd version webpack and remove dotenv plugin

--- config/webpack/constants/path.ts
+++ config/webpack/constants/path.ts
@@ -6,8 +6,6 @@ export const paths = {
     entry        : path.resolve('src/core/index.tsx'),
     template     : path.resolve('src/core/index.pug'),
     view         : path.resolve('dist/index.pug'),
-    env          : path.resolve('./.env'),
-    envExample   : path.resolve('./.env.example'),
     postCssConfig: path.resolve('config/css/.postcssrc'),
     fonts        : path.resolve('assets/fonts'),
     images       : path.resolve('assets/images'),
