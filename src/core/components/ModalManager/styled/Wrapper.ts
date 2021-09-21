31283b216cfaa9bfd4849ce8eb091c04a3b6c42b
kirill-krasuk <kirkras7356@gmail.com>
2021-09-21 16:03:52 +0300

    style: configure stylelint for {css,sass} and styled components

--- src/core/components/ModalManager/styled/Wrapper.ts
+++ src/core/components/ModalManager/styled/Wrapper.ts
@@ -2,27 +2,27 @@ import styled from 'styled-components/macro';
 
 export const Wrapper = styled.div`
     position: absolute;
+    z-index: 999;
     top: 0;
     left: 0;
+    display: flex;
     width: 100%;
     height: calc(100% - 300px);
-    z-index: 999;
-    color: white;
-    display: flex;
-    justify-content: center;
     align-items: flex-start;
+    justify-content: center;
     padding-top: 300px;
     backdrop-filter: blur(5px);
+    color: white;
 
     &:before {
-        content: '';
         position: absolute;
         z-index: 1000;
-        background-color: black;
-        filter: opacity(.3);
+        top: 0;
         width: 100%;
         height: 100%;
-        top: 0;
+        background-color: black;
+        content: '';
+        filter: opacity(.3);
     }
 
     & > div {
