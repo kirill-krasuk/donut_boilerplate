9d5d6b37a38861e8249648ba6fd383c99a43e0c8
kirill-krasuk <naviinsane@gmail.com>
2021-09-29 23:13:49 +0300

    ci(circleci): fix jest config

--- bin/circleci_test.sh
+++ bin/circleci_test.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
+source ./bin/variables.sh
+
 TEST=$(circleci tests glob **/__tests__/*.tsx? | circleci tests split --split-by=timings)
-yarn jest $TEST
+yarn jest -c=$JEST_CONFIG_PATH $TEST
