f18cf77a86005362f04baad4dfebd39688d54bff
kirill-krasuk <kirkras7356@gmail.com>
2021-10-07 18:40:30 +0300

    style(storybook): update storybook addons

--- src/ui-kit/components/Button/index.stories.tsx
+++ src/ui-kit/components/Button/index.stories.tsx
@@ -1,18 +1,46 @@
+import {
+    withKnobs, text, boolean
+} from '@storybook/addon-knobs';
+import { BADGE }        from '@geometricpanda/storybook-addon-badges';
+
 import { ButtonStyles } from '@ui-kit/enums/button';
 import { Button }       from './component';
 
 export default {
-    title     : 'Button',
+    title     : 'Design System/Atoms/Button',
     component : Button,
+    decorators: [ withKnobs ],
+    argTypes  : {
+        onClick: {
+            action: 'clicked'
+        }
+    },
     parameters: {
-        componentSubtitle: 'Simple button representing'
-    }
+        badges: [ BADGE.EXPERIMENTAL ],
+        a11y  : {
+            config: {
+                rules: [ { id: 'listitem', enabled: false } ],
+            },
+        },
+    },
 };
 
-export const withText = () => <Button style={ ButtonStyles.Primary }>Hello</Button>;
+export const withText = (args: any) => {
+    const isLoading = boolean('isLoading', false);
 
-export const withEmoji = () => (
-    <Button>
+    return (
+        <Button { ...args } style={ ButtonStyles.Primary }>
+            {
+                isLoading
+                    ? 'loading...'
+                    : text('content', 'Hello')
+            }
+        </Button>
+    );
+};
+
+export const withEmoji = (args: any) => (
+    <Button { ...args }>
         <span role="img" aria-label="so cool">
             😀 😎 👍 💯
         </span>
