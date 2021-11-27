2a19a3dc14cf85fec5fa16677fc92983ddabc2cb
kirill-krasuk <naviinsane@gmail.com>
2021-11-27 16:49:36 +0200

    refactor(strings): reimplement interpolate method

--- src/utils/string/interpolate.ts
+++ src/utils/string/interpolate.ts
@@ -1,9 +1,31 @@
-/**
- * TODO: re implement
- */
+const escapeRegex = /{{(\s)?\$([0-9]+)(\s)?}}/g;
 
-// const escapeRegex = /{{(\s)?\$([0-9]+)(\s)?}}/g;
+const normalizeValues = (args: string[]): number[] => args
+    .map(arg => parseInt(arg.replace(escapeRegex, '$2'), 10));
 
-// const normalizeValues = (args: string[]): number[] => args.map(arg => parseInt(arg.replace(escapeRegex, '$2'), 10));
+const replace = (str: string, args: string[] | null, valuesToReplace: ReadonlyArray<string | number>, index = 0): string => {
+    if (args) {
+        if (args.length <= index) {
+            return str;
+        }
 
-export const interpolate = () => undefined;
+        const replaceValue = valuesToReplace[normalizeValues(args!)[index]].toString();
+
+        return replace(
+            str.replace(args![index], replaceValue),
+            args,
+            valuesToReplace,
+            index + 1
+        );
+    }
+
+    return str;
+};
+
+export const interpolate = (message: string | null, values: ReadonlyArray<string | number>, defaultMessage?: string): string => {
+    const messageToReplace = message
+        ? message
+        : defaultMessage || '';
+
+    return replace(messageToReplace, messageToReplace.match(escapeRegex), values);
+};
