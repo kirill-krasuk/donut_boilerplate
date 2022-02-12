9fb3f839cfcb03deaea8088d34231c40256393a7
kirill-krasuk <kirkras7356@gmail.com>
2022-02-12 03:07:03 +0200

    fix(styles): fix isomorphic set theme

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,5 +1,4 @@
-export { default as Root } from './Root';
-
 export { default as Home } from './Home';
 export { default as Second } from './Second';
 export { default as Header } from './Header';
+export { default as GlobalStyles } from './GlobalStyles';
--- src/core/components/Root/hooks/useTheme.ts
+++ src/core/components/Root/hooks/useTheme.ts
@@ -1,22 +1,13 @@
-/* eslint-disable no-param-reassign */
-import { useEffect, FC, useState }            from 'react';
+import { useState, useEffect }                from 'react';
 import { useSelector }                        from 'react-redux';
-import { Outlet }                             from 'react-router-dom';
-import { ThemeProvider }                      from 'styled-components/macro';
 import Cookie                                 from 'js-cookie';
 
-import { theme as themes }                    from '@core/config/theme';
-import { getMode }                            from '@core/store/selectors/theme';
-import { GlobalStyles }                       from '@app/components/GlobalStyles';
-import LanguageProvider                       from '@core/components/LanguageProvider';
-
-// import ModalManager                           from '@core/components/ModalManager';
 import { Theme }                              from '@core/enums/theme';
 import { useActions }                         from '@core/hooks/useActions';
-import { getDarkModeQuery, setDataThemeAttr } from '@utils/dom';
-import * as S                                 from './styles';
+import { getMode }                            from '@core/store/selectors/theme';
+import { setDataThemeAttr, getDarkModeQuery } from '@utils/dom';
 
-const Root: FC = () => {
+export function useTheme() {
     const mode = useSelector(getMode);
 
     const [ theme, setTheme ] = useState(mode);
@@ -57,24 +48,5 @@ const Root: FC = () => {
         }
     });
 
-    return (
-        <ThemeProvider theme={ {
-            ...themes,
-            mode  : theme,
-            isDark: theme === Theme.Dark
-        } }
-        >
-            <S.Wrapper>
-                <GlobalStyles />
-                <LanguageProvider>
-                    <>
-                        <Outlet />
-                        { /* <ModalManager /> */ }
-                    </>
-                </LanguageProvider>
-            </S.Wrapper>
-        </ThemeProvider>
-    );
-};
-
-export default Root;
+    return theme;
+}
