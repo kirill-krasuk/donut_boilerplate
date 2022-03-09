e6d350400840b4c9c72fbd4b5da2e9239794686d
kirill-krasuk <naviinsane@gmail.com>
2022-03-09 19:51:02 +0200

    refactor(lint): add interface rule for semicolon member end

--- config/env/env.d.ts
+++ config/env/env.d.ts
@@ -1,20 +1,20 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            APP_NAME?: string,
-            NODE_ENV?: 'development' | 'production',
-            HOST?: string,
-            PORT?: number,
-            API_HOST?: string,
-            SERVICE_WORKER_ENABLE?: boolean,
-            HYDRATE?: boolean,
-            OPEN_IN_BROWSER?: boolean,
-            WRITE_ASSETS_TO_DISK?: boolean,
-            DOCKER_TAG?: string,
-            DOCKER_CONFIG_PATH?: string,
-            DOCKER_CONTAINER_NAME?: string,
-            BUILD_ANALYZE?: boolean,
-            BUNDLE_ANALYZER_PORT?: number
+            APP_NAME?: string;
+            NODE_ENV?: 'development' | 'production';
+            HOST?: string;
+            PORT?: number;
+            API_HOST?: string;
+            SERVICE_WORKER_ENABLE?: boolean;
+            HYDRATE?: boolean;
+            OPEN_IN_BROWSER?: boolean;
+            WRITE_ASSETS_TO_DISK?: boolean;
+            DOCKER_TAG?: string;
+            DOCKER_CONFIG_PATH?: string;
+            DOCKER_CONTAINER_NAME?: string;
+            BUILD_ANALYZE?: boolean;
+            BUNDLE_ANALYZER_PORT?: number;
         }
     }
 }
