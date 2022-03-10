0b7786c2394605e8fa964c965faf93706312e4ef
kirill-krasuk <naviinsane@gmail.com>
2022-03-10 13:24:56 +0200

    feat(types): add couple utility types

--- src/shared/types/utility.ts
+++ src/shared/types/utility.ts
@@ -1,3 +1,17 @@
 export type InferValueTypes<T> = T extends Record<string, infer U>
     ? U
     : never;
+
+export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
+
+export type LastOf<T> = UnionToIntersection<T extends any ? () => T : never> extends () => (infer R) ? R : never
+
+export type Push<T extends any[], V> = [...T, V];
+
+export type TuplifyUnion<T, L = LastOf<T>, N = [T] extends [never] ? true : false> =
+    true extends N ? [] : Push<TuplifyUnion<Exclude<T, L>>, L>
+
+export type ObjValueTuple<T, KS extends any[] = TuplifyUnion<keyof T>, R extends any[] = []> =
+    KS extends [infer K, ...infer KT]
+        ? ObjValueTuple<T, KT, [...R, T[K & keyof T]]>
+        : R
