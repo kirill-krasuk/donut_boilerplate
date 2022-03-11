80af01d035ea82a52eea454e02cef96ac8808245
kirill-krasuk <naviinsane@gmail.com>
2022-03-11 16:25:50 +0200

    fix(server): fix compilation app down with wepback stats

--- server/utils/createCompilationPromise.ts
+++ server/utils/createCompilationPromise.ts
@@ -1,12 +1,16 @@
 import webpack from 'webpack';
 
 export function createCompilationPromise(compiler: webpack.Compiler): Promise<void> {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         compiler.hooks.done.tap('dev', (stats) => {
             if (!stats.hasErrors()) {
                 resolve();
             } else {
-                reject();
+                // make delay for server down with error
+                // end output compilation status in stdout
+                setTimeout(() => {
+                    process.exit(1);
+                }, 100);
             }
         });
     });
