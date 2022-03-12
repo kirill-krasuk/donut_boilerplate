2308dadefa90d3e63731b96e268fc0e3bd085fdc
kirill-krasuk <naviinsane@gmail.com>
2022-03-13 01:19:46 +0200

    fix(storybook): storybook build

--- config/.storybook/preview.js
+++ config/.storybook/preview.js
@@ -4,7 +4,7 @@ import { addDecorator }      from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { ThemeProvider }     from 'styled-components/macro';
 
-import { theme }             from '@app/theme';
+import { theme }             from '@config/theme';
 
 addDecorator((storyFn) => (
     <ThemeProvider theme={ { ...theme, mode: 'dark' } }>
