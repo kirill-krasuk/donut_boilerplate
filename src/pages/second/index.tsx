d0429f97060333dfb76e210e8770d7f6a95638f3
kirill-krasuk <naviinsane@gmail.com>
2022-03-24 02:09:30 +0200

    refactor: tru prefetch in webpack for code spliting

--- src/pages/second/index.tsx
+++ src/pages/second/index.tsx
@@ -1,7 +1,7 @@
 import loadable from '@loadable/component';
 
 export const Second = loadable(
-    () => import(/* webpackChunkName: "Second" */'./ui/Second'),
+    () => import(/* webpackChunkName: "Second", webpackPrefetch: true */'./ui/Second'),
     {
         fallback: <div>Loading...</div>
     }
