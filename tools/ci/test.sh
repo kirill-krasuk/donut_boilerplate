c0675bc78367e9c1daebfb3f3b01488b60e1d044
kirill-krasuk <naviinsane@gmail.com>
2022-03-31 00:50:49 +0300

    fix(ci): try to fix ci

--- tools/ci/test.sh
+++ tools/ci/test.sh
@@ -10,9 +10,10 @@ source ./tools/constants/colors.sh
 
 echo -e "${COLOR_BOLD_BLACK}${BACKGROUND_COLOR_BLUE} INFO ${COLOR_OFF} Run tests suites"
 
+# shellcheck disable=SC2086
 case $1 in
-    w) yarn jest --env=jsdom --watch -c="$JEST_CONFIG_PATH";;
-    c) yarn jest --env=jsdom --coverage -c="$JEST_CONFIG_PATH";;
-    u) yarn jest --env=jsdom -u -c="$JEST_CONFIG_PATH";;
-    *) yarn jest --runInBand --env=jsdom -c="$JEST_CONFIG_PATH";;
+    w) yarn jest --env=jsdom --watch -c=$JEST_CONFIG_PATH;;
+    c) yarn jest --env=jsdom --coverage -c=$JEST_CONFIG_PATH;;
+    u) yarn jest --env=jsdom -u -c=$JEST_CONFIG_PATH;;
+    *) yarn jest --runInBand --env=jsdom -c=$JEST_CONFIG_PATH;;
 esac
