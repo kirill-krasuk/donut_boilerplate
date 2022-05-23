ab3189376199004f068fc92ffc58e1c2ba1d5bd6
kirill-krasuk <naviinsane@gmail.com>
2022-05-23 18:11:35 +0300

    refactor: fix global styles by stylelint

--- src/app/providers/with-styles/GlobalStyles.ts
+++ src/app/providers/with-styles/GlobalStyles.ts
@@ -7,53 +7,54 @@ import {
 } from '@config/theme';
 
 const GlobalStyles = createGlobalStyle`
-    .light {
-        ${ lightValues }
-    }
+	.light {
+		${ lightValues }
+	}
 
-    .dark {
-        ${ darkValues }
-    }
+	.dark {
+		${ darkValues }
+	}
 
-    :root {
-        ${ commonValues }
-    }
+	:root {
+		${ commonValues }
+	}
 
-    * {
-        margin: 0;
-        padding: 0;
-        font-family: 'Montserrat', sans-serif;
-        transition: background-color .5s, color .5s ease;
-    }
+	* {
+		margin: 0;
+		padding: 0;
+		font-family: Montserrat, sans-serif;
+		transition: background-color .5s, color .5s ease;
+	}
 
-    html, body {
-        min-height: 100vh;
-        overflow-x: hidden;
-        font-size: 14px;
-        background-color: var(--background);
-    }
+	html,
+	body {
+		min-height: 100vh;
+		overflow-x: hidden;
+		font-size: 14px;
+		background-color: var(--background);
+	}
 
-    #root {
-        min-height: inherit;
-    }
+	#root {
+		min-height: inherit;
+	}
 
-    code {
-        color: var(--primary);
-        font-size: inherit;
-        font-family: monospace;
-    }
+	code {
+		color: var(--primary);
+		font-size: inherit;
+		font-family: monospace;
+	}
 
-    p > code,
-    li > code,
-    dd > code,
-    td > code,
-    span > code {
-        padding: .1rem .3rem .2rem;
-        word-wrap: break-word;
-        background: #ffeff0;
-        border-radius: .2rem;
-        box-decoration-break: clone;
-    }
+	p > code,
+	li > code,
+	dd > code,
+	td > code,
+	span > code {
+		padding: .1rem .3rem .2rem;
+		word-wrap: break-word;
+		background: #ffeff0;
+		border-radius: .2rem;
+		box-decoration-break: clone;
+	}
 `;
 
 export { GlobalStyles };
