b822e7942b8683d4825b8f16a2ec4cd75e9846c5
kirill-krasuk <naviinsane@gmail.com>
2022-12-09 15:00:40 +0200

    refactor: better experience with react context

--- src/shared/hooks/useInitialProps.ts
+++ src/shared/hooks/useInitialProps.ts
@@ -3,7 +3,13 @@ import { useContext }      from 'react';
 import { redirectContext } from '@shared/context/redirect-context';
 
 function useInitialProps<T>() {
-	return useContext<T>(redirectContext);
+	const context = useContext<T>(redirectContext);
+
+	if (!context) {
+		throw new Error('useInitialProps must be used within a RedirectContextProvider');
+	}
+
+	return context;
 }
 
 export { useInitialProps };
