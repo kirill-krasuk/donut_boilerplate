f785ac8387ec187d0ecd2634b6b838884bd7a6a5
kirill-krasuk <naviinsane@gmail.com>
2023-07-13 20:39:53 +0300

    refactor: changes in configs and add tests

---
+++ config/env/generateConfig.spec.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+import { generateConfig }    from './generateConfig';
+
+import type { ConfigSchema } from './types';
+
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			VAR1: string;
+			VAR2: string;
+			VAR3: string;
+		}
+	}
+}
+
+describe('generateConfig', () => {
+	it('should return empty object if schema and env is empty', () => {
+		const env    = {} as NodeJS.ProcessEnv;
+		const schema = {};
+
+		const result = generateConfig(env)(schema);
+
+		expect(result).toStrictEqual({});
+	});
+
+	it('should result contains from nulls', () => {
+		const env    = {} as NodeJS.ProcessEnv;
+		const schema = {
+			a: () => null ?? '1',
+			b: () => null ?? 'false',
+			c: () => null ?? 'hello'
+		};
+
+		const result = generateConfig(env)(schema);
+
+		expect(result).toStrictEqual({ a: 1, b: false, c: 'hello' });
+	});
+
+	it('should transform "number" => number', () => {
+		const env                  = { VAR1: '1', VAR2: '2', VAR3: '3' };
+		const schema: ConfigSchema = {
+			a: env => env.VAR1,
+			b: env => env.VAR2,
+			c: env => env.VAR3
+		};
+
+		const result = generateConfig(env)(schema);
+
+		expect(result).toStrictEqual({ a: 1, b: 2, c: 3 });
+	});
+});
--- config/env/types.ts
+++ config/env/types.ts
@@ -2,8 +2,16 @@ type ConfigSchema = Readonly<
 	Record<string, (env: NodeJS.ProcessEnv) => boolean | number | string | null>
 >;
 
+type NormalizedValue<T extends boolean | number | string | null> = T extends string
+	? T extends `${ number }`
+		? number
+		: T
+	: T extends `${ boolean }`
+	? boolean
+	: T;
+
 type Config<T extends ConfigSchema> = {
-	readonly [key in keyof T]: ReturnType<T[key]>;
+	readonly [key in keyof T]: NormalizedValue<ReturnType<T[key]>>;
 };
 
 export type { ConfigSchema, Config };
